b7cd7968325eb1d4959a4c420c9c024ab75a5a57
IwanIDev <iwan@iwani.dev>
2026-02-27 14:57:30 +0000

    Changed blog URL to a subdomain

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ MARKDOWN = MARKDOWN
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://www.iwaningman.com"
+SITEURL = "https://blog.iwaningman.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
